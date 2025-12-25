56e5965026e61921f8eeeba01179ee9c0c8e5372
AkshataThorvat <akshatathorvat1001@gmail.com>
2025-12-25 10:51:23 +0530

    assignment 10

--- assignment10/MQTT__Subscriber/MQTT__Subscriber.ino
+++ assignment10/MQTT__Subscriber/MQTT__Subscriber.ino
@@ -1,70 +1,56 @@
-#include<WiFiClient.h>
 #include<WiFi.h>
 #include<ArduinoMqttClient.h>
 
-const char *ssid = "SUNBEAM";
-const char *password = "1234567890";
+const char *ssid = "Vivo y 100a";
+const char *password = "89670555";
 
-const char *host = "172.18.4.146";
-const int port = 1883;
+const char *host = "test.mosquitto.org";
+unsigned int port = 1883;
 
 WiFiClient wifiClient;
 MqttClient subscriber(wifiClient);
 
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.flush();
 
   pinMode(2, OUTPUT);
-  digitalWrite(2, LOW);
-  
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
-  Serial.println("Connecting to a WiFi");
+  Serial.print("Connecting to WiFi ");
   while(WiFi.status() != WL_CONNECTED){
-    Serial.print(".");
     delay(500);
+    Serial.print(".");
   }
-
-  Serial.println("\n WiFi is connected");
+  Serial.println("Connected to WiFi");
   Serial.print("IP Address : ");
   Serial.println(WiFi.localIP());
 
-  if(!subscriber.connect(host, port))
+  if(!subscriber.connect(host, port)){
     while(1);
-
-  Serial.println("Connected to the Broker");
-
+  }
+  Serial.println("Subscriber is connected to broker");
   subscriber.subscribe("room/light");
-
-  Serial.println("Topic is subscribed");
-  
+  Serial.println("Subscribed to topic : room/light");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int size = subscriber.parseMessage();
-  if(size){
-    char str[size];
-    memset(str, 0, sizeof(str));
-    for(int i = 0 ; i < size ; i++)
-      str[i] = (char)subscriber.read();
-
-    Serial.printf("Message : %s\n",str);
+  int len = subscriber.parseMessage();
+  if(len){
+    char str[len];
+    for(int i = 0 ; i < len ; i++)
+      str[i] = subscriber.read();
 
     if(strncmp(str, "ON", 2) == 0){
       digitalWrite(2, HIGH);
-      Serial.println("LED is Turned ON");
+      Serial.println("LED is truned ON");
     }
     else if(strncmp(str, "OFF", 3) == 0){
       digitalWrite(2, LOW);
-      Serial.println("LED is Turned OFF");
+      Serial.println("LED is truned OFF");
     }
-
   }
-
-  delay(6000);
 }
